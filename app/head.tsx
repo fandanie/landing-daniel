45ef3a2911d837bfe1a110de896341890d427c21
Daniel Fandino <daniel.fandino.baez@gmail.com>
2025-07-14 12:31:04 -0500

    update 6

--- app/head.tsx
+++ app/head.tsx
@@ -9,28 +9,3 @@ export default function Head() {
 }
 
 
-export const metadata = {
-    title: "Daniel Fandiño | Desarrollador Web",
-    description: "Soy desarrollador web con experiencia en Java, React, micro frontends y Spring Boot.",
-    openGraph: {
-        title: "Daniel Fandiño | Desarrollador Web",
-        description: "Portafolio profesional con proyectos, habilidades y contacto.",
-        url: "https://landing-daniel.vercel.app", // actualiza si tienes dominio propio
-        siteName: "Daniel Fandiño",
-        images: [
-            {
-                url: "/preview.png",
-                width: 1200,
-                height: 630,
-                alt: "Daniel Fandiño | Desarrollador Web",
-            },
-        ],
-        type: "website",
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: "Daniel Fandiño | Desarrollador Web",
-        description: "Portafolio profesional con proyectos, habilidades y contacto.",
-        images: ["/preview.png"],
-    },
-};
